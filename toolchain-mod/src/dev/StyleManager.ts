1f2f75e7ab832e68c5e8936773bd74104c27893d
Dmitriy Medvedev <ddcompanychanel@gmail.com>
2020-08-30 10:45:38 +0300

    Rewritten in TS

--- toolchain-mod/src/dev/StyleManager.ts
+++ toolchain-mod/src/dev/StyleManager.ts
@@ -1,53 +1,25 @@
-let StyleManager = {
-    /**
-     * Зарегистрированные стили. Ключом является название
-     */
-    styles: {},
+class StyleManager {
+    static styles = {};
 
-    /**
-     * Регистрация стиля
-     * @param name название
-     * @param obj объект описания
-     */
-    add: function (name, obj) {
+    static add(name: string, obj: any) {
         this.styles[name] = obj;
-    },
+    }
 
-    /**
-     * Применение стандартного стиля
-     */
-    applyDefault: function () {
-        Style = {};
-        for (let i in DEFAULT_STYLE) {
-            Style[i] = DEFAULT_STYLE[i];
-        }
-    },
-
-    /**
-     * Применение стиля
-     * @param name название стиля
-     */
-    apply: function (name) {
+    static apply(name: string) {
         let style = this.styles[name];
         if (!style) {
-            this.applyDefault();
             Logger.Log("Style " + name + " is not found! Default was applied", "ERROR");
             return;
         }
-
-        this.applyDefault();
 
         for (let i in style) {
             Style[i] = style[i];
         }
 
         Logger.Log("Waila Style: " + name, "INFO");
-    },
+    }
 
-    /**
-     * Чтение стилей из JSON файла
-     */
-    readFromFile: function () {
+    static readFromFile() {
         let content = FileTools.ReadText(__dir__ + "json/styles.json");
 
         if (content) {
@@ -63,10 +35,12 @@ let StyleManager = {
 
                 this.styles[i] = style;
             }
-        } else Logger.Log("json/styles.json is not found!", "ERROR");
-    },
+        } else {
+            Logger.Log("json/styles.json is not found!", "ERROR");
+        }
+    }
 
-    compileColor: function (obj, field) {
+    static compileColor(obj: any, field: string) {
         let style = obj[field];
 
         if (!style) {
@@ -75,7 +49,7 @@ let StyleManager = {
 
         obj[field] = Color.argb(style.a || 255, style.r, style.g, style.b);
     }
-};
+}
 
 StyleManager.readFromFile();
 StyleManager.apply(WailaConfig.style);
